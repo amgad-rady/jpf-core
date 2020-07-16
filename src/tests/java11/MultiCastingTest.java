96c3d3b692a6d4429f813345c14b18cb89b8f46e
amgad-rady <amgad.s.rady@gmail.com>
2020-07-16 16:53:21 -0400

    Added another failing multicasting altMetafactory test

--- src/tests/java11/MultiCastingTest.java
+++ src/tests/java11/MultiCastingTest.java
@@ -4,10 +4,8 @@ import gov.nasa.jpf.util.test.TestJPF;
 import org.junit.Test;
 
 import java.io.Serializable;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.*;
+import java.util.function.ToIntFunction;
 
 public class MultiCastingTest extends TestJPF {
   public interface Area {
@@ -39,28 +37,43 @@ public class MultiCastingTest extends TestJPF {
     }
   }
 
-  @Test
-  public void testShapeCast() {
-    Object sq = new UnitSquare();
-    sq = (Area & Circumference & Dimension) sq;
-  }
+//  @Test
+//  public void testShapeCast() {
+//    Object sq = new UnitSquare();
+//    sq = (Area & Circumference & Dimension) sq;
+//  }
 
   public static <K, V extends Comparable<? super V>> Comparator<Map.Entry<K, V>> comparingByValue() {
-    return
+    return (Comparator<Map.Entry<K, V>> & Serializable)
       (c_1, c_2) -> c_1.getValue().compareTo(c_2.getValue());
   }
 
+  public static <T> Comparator<T> comparingInt(ToIntFunction<? super T> keyExtractor) {
+    return (Comparator<T> & Serializable)
+      (c1, c2) -> Integer.compare(keyExtractor.applyAsInt(c1), keyExtractor.applyAsInt(c2));
+  }
+
+//  @Test
+//  public void testMapEntryMethod() {
+//    if (verifyNoPropertyViolation()) {
+//      Map<Integer, Integer> map = new HashMap<>();
+//      map.put(0, 1);
+//      map.put(1, 2);
+//      Iterator<Map.Entry<Integer, Integer>> entries = map.entrySet().iterator();
+//      Map.Entry<Integer, Integer> first = entries.next();
+//      Map.Entry<Integer, Integer> second = entries.next();
+//      Comparator<Map.Entry<Integer, Integer>> lambda = comparingByValue();
+//      int result = lambda.compare(first, second);
+//    }
+//  }
+
   @Test
-  public void testMapEntryMethod() {
+  public void testComparingInt() {
     if (verifyNoPropertyViolation()) {
-      Map<Integer, Integer> map = new HashMap<>();
-      map.put(0, 1);
-      map.put(1, 2);
-      Iterator<Map.Entry<Integer, Integer>> entries = map.entrySet().iterator();
-      Map.Entry<Integer, Integer> first = entries.next();
-      Map.Entry<Integer, Integer> second = entries.next();
-      Comparator<Map.Entry<Integer, Integer>> lambda = comparingByValue();
-      int result = lambda.compare(first, second);
+      ToIntFunction<Integer> keyExtractor = (x) -> 0;
+      Comparator<Integer> comparator = comparingInt(keyExtractor);
+      int result = comparator.compare(0, 1);
+      System.out.println("Result: " + result);
     }
   }
 }
